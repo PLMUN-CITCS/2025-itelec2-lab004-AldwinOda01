9581d9411545b835b50a62354a613902872ac4fa
AldwinOda01 <>
2025-03-14 15:21:09 +0800

    Last Sync: 2025-03-14 07:21:09 (Mobile)

---
+++ exercise_02.py
@@ -0,0 +1,28 @@
+# Aldwin Odavar
+# ITELEC2
+# Laboratory #03 – Guided Coding Exercise:
+# Variables, Literals, and Case-Sensitivity in Python (with Naming Conventions)
+
+count = 10             
+Count = 15             
+total_count = 20      
+
+decimal_value = 3.14      
+
+message = "Hello, Python!"   
+
+is_active = True        
+
+result = None           
+
+print("Integer (count):", count)
+print("Integer (Count):", Count)
+print("Integer (total_count):", total_count)
+print("Decimal:", decimal_value)
+print("Text:", message)
+print("Boolean:", is_active)
+print("None Value:", result)
+
+num1 = 5
+num2 = 3
+print(f"Sum: {num1 + num2:.2f}")
